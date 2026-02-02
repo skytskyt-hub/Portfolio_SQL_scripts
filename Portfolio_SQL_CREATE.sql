ea60367954ae95ee828f77fd92efb96bf66a5c2a
Emilie Skytta <243698059+skytskyt-hub@users.noreply.github.com>
2026-02-02 14:10:56 -0800

    Add files via upload

---
+++ Portfolio_SQL_CREATE.sql
@@ -0,0 +1,43 @@
+CREATE TABLE users (
+user_id integer PRIMARY KEY NOT NULL,
+firstname TEXT, 
+visits integer,
+clicks integer, 
+status TEXT,
+created_date TIMESTAMP, 
+country TEXT,
+marketing_opt_in boolean
+);
+
+SELECT *
+FROM users
+LIMIT 5;
+
+CREATE TABLE products (
+product_id integer PRIMARY KEY NOT NULL,
+product_name varchar(60),
+price_monthly float,
+annual_disc_amount float,
+description varchar,
+is_active boolean
+);
+
+SELECT *
+FROM products
+LIMIT 5;
+
+CREATE TABLE subscriptions (
+subscription_id integer PRIMARY KEY NOT NULL,
+user_id integer REFERENCES users (user_id),
+product_id integer REFERENCES products (product_id),
+signup_date TIMESTAMP NOT NULL,
+purchase_date TIMESTAMP,
+billing_period TEXT,
+annual_discount boolean,
+churn_date TIMESTAMP,
+trial_period_days numeric
+);
+
+SELECT *
+FROM subscriptions
+LIMIT 5;
