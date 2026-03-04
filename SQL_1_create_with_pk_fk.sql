4a443ffcf12452a7f6db54a56c057f1466b28570
Emilie Skytta <243698059+skytskyt-hub@users.noreply.github.com>
2026-03-03 16:46:14 -0800

    Update SQL_1_create_with_pk_fk.sql

--- SQL_1_create_with_pk_fk.sql
+++ SQL_1_create_with_pk_fk.sql
@@ -10,9 +10,16 @@ country TEXT,
 marketing_opt_in boolean
 );
 
-SELECT *
-FROM users
-LIMIT 5;
+/* demonstrates how data is inserted via SQL. Actual method used: Imported from csv with 
+  mapping due to using ai generated datasets in csv format */
+INSERT INTO users (user_id, firstname, visits, clicks, status, created_date, country, marketing_opt_in)
+VALUES 
+  (924197,'taylor',14,38,'active',2024-02-10 22:44:38.421243,NULL,False),
+  (562958,'Alex',15,37,'inactive',2024-09-21 22:44:38.421269,'US',False),
+  (381632,'Avery',11,37,'INACTIVE',2025-10-20 22:44:38.421274,'US',NULL)
+  -- continue with all data ... 
+  ;
+
 
 --Establishes primary key
 CREATE TABLE products (
@@ -23,10 +30,7 @@ annual_disc_amount float,
 description varchar,
 is_active boolean
 );
-
-SELECT *
-FROM products
-LIMIT 5;
+--csv data imported and mapped in DBeaver
 
 --Establishes pk and foreign keys to create the sample relational database
 CREATE TABLE subscriptions (
@@ -40,7 +44,4 @@ annual_discount boolean,
 churn_date TIMESTAMP,
 trial_period_days numeric
 );
-
-SELECT *
-FROM subscriptions
-LIMIT 5;
+--csv data imported and mapped in DBeaver
